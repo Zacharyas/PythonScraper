da01fdbb6e8802ce9efdad087224fd664573ada2
Zack <zekaryas1zekaryasasferie@gmail.com>
2019-01-29 19:51:27 -0500

    first message

--- MainWindow.py
+++ MainWindow.py
@@ -1,11 +1,13 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit
 app = QApplication([])
+app.setStyle('Fusion')
 window = QWidget()
 layout = QVBoxLayout()
 adress = QLineEdit()
 submitButton = QPushButton("Submit")
 layout.addWidget(adress)
 layout.addWidget(submitButton)
+
 window.setLayout(layout)
 window.show()
 app.exec_()
