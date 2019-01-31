75c32db0999d3ba244958cc9f824bb6ebf11ef01
Zack <zekaryas1zekaryasasferie@gmail.com>
2019-01-30 20:03:50 -0500

    insert your username

--- MainWindow.py
+++ MainWindow.py
@@ -4,7 +4,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 import random
-
+import os
 app = QApplication([])
 app.setStyle('Fusion')
 window = QWidget()
@@ -12,21 +12,23 @@ layout = QGridLayout()
 window.setLayout(layout)
 adress = QLineEdit()
 adress2 = QLineEdit()
-
+picNum = random.randint(20,900)
 
 def Downloader():
     getAdress = requests.get(adress.text())
     image = Image.open(BytesIO(getAdress.content))
-    picNum = random.randint(20,900)
-    image.save('C:/Users/Zekaryas/Desktop/savefile/landscape%d.png'%(picNum), 'PNG')
+    image.save('C:/Users/yourusername/Desktop/Scraper/landscape%d.png'%(picNum), 'PNG')
+    print("downloaded")
+
 directDownload = QLabel("directly download from an image url")
 layout.setSpacing(10)
 submitButton = QPushButton("Download")
-submitButton2 = QPushButton("Download")
+submitButton2 = QPushButton("Download2")
 layout.addWidget(directDownload)
 
 layout.addWidget(adress,0,1)
 layout.addWidget(submitButton,0,2)
+submitButton.clicked.connect(Downloader)
 #facebook
 directDownload = QLabel("directly download from facebook")
 layout.setSpacing(10)
@@ -37,7 +39,7 @@ layout.addWidget(directDownload,2,0)
 layout.addWidget(adress2,2,1)
 layout.addWidget(submitButton2,2,2)
 
-submitButton.clicked.connect(Downloader)
+
 window.show()
 app.exec_()
 
